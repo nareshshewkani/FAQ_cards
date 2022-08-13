cfd089951de5118a947899ac13a3d9c66f0ea1e8
nareshshewkani <98037929+nareshshewkani@users.noreply.github.com>
2022-08-13 20:24:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let toggleButtons = document.querySelectorAll(".faq-toggle");
+
+toggleButtons.forEach(button => {
+  button.addEventListener("click",() => {
+    button.parentNode.classList.toggle("active");
+  });
+});
